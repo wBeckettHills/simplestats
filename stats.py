2f5f309513ee50dfbaf256ba2f02d8a770e8fe24
Beckett Hills <whills@wisc.edu>
2016-01-15 13:27:45 -0600

    added a function test_mean

--- stats.py
+++ stats.py
@@ -7,6 +7,10 @@ def mean(vals):
     total = sum(vals)
     length = len(vals)
     return total/length
-    
+
+def test_mean():
+    assert (mean([2,4])) == 3.0
+test_mean()
+
 #print(mean([2,4]))
-print(mean("hello"))
+#print(mean("hello"))
