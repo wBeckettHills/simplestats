2d36f95f2a0ac3f23d03f801a8030c5669668782
Beckett Hills <whills@wisc.edu>
2016-01-15 13:40:51 -0600

    modified to add if else

--- stats.py
+++ stats.py
@@ -3,14 +3,21 @@
 def mean(vals):
     #Calculate the arithmetic mean of a list
     assert type(vals) is list, 'wrong input format'
-
+    
     total = sum(vals)
     length = len(vals)
-    return total/length
+    if length == 0:
+        return 0.0
+    else:
+        return total/length
 
 def test_mean():
     assert (mean([2,4])) == 3.0
 test_mean()
+
+def test_empty_list():
+    assert mean([]) == 0.0
+test_empty_list()
 
 #print(mean([2,4]))
 #print(mean("hello"))
