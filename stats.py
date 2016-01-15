3c70baeeb030ebfe6d881e78bcbdcfa51ca3c287
Beckett Hills <whills@wisc.edu>
2016-01-15 13:22:22 -0600

    added assertion to catch errors

--- stats.py
+++ stats.py
@@ -2,8 +2,11 @@
 
 def mean(vals):
     #Calculate the arithmetic mean of a list
+    assert type(vals) is list, 'wrong input format'
+
     total = sum(vals)
     length = len(vals)
     return total/length
     
-print(mean([2,4]))
+#print(mean([2,4]))
+print(mean("hello"))
