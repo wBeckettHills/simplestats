9c01deab8cc4350569c28664647cf11beb9a7648
Beckett Hills <whills@wisc.edu>
2016-01-15 14:03:31 -0600

    added assert_equal from nose.tools

--- test_stats.py
+++ test_stats.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 from stats import mean
+from nose.tools import assert_equal,assert_almost_equals
 
 def test_mean():
-    assert (mean([2,4])) == 3.0
-test_mean()
+    assert_equal(mean([2,4]),3.0)
+#test_mean()
 
 def test_empty_list():
-    assert mean([]) == 0.0
-test_empty_list()
+    assert_equal(mean([]),0.0)
+#test_empty_list()
 
 def test_float_mean():
-    assert (mean([5,6])) == 5.5
-test_float_mean()
+    assert_almost_equals(mean([5,6]),5.5)
+#test_float_mean()
